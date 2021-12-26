9b7b3d0aa63ec7d35b75c9198a11dcc246fb77d7
lichang <2217024156@qq.com>
2021-12-26 11:35:15 +0800

    new

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,9 @@
-import React from "react"
+import React from "react";
+import {Link} from "gatsby"
 
 export default ({ children }) => (
   <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+   <Link to="/about">关于我们</Link>
     {children}
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,8 @@ import Main from "../components/main"
 import * as mainStyle from "./main.module.css"
 
 export default function Home() {
-  console.log(mainStyle)
-  return (
-    <Main>
-      <div className="">主页面</div>
-    </Main>
+  return ( <Main >
+    <div className = {mainStyle.app}> 主页面 </div>  
+    </Main >
   )
 }
